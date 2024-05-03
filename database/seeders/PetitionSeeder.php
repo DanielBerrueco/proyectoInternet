69c3e24546fe4595f8cf7ee22ebf8ba0fbd12823
Sergio Gutierrez <sg7045167@gmailcom>
2024-05-03 15:27:55 -0600

    Creación seeder Petición

--- database/seeders/PetitionSeeder.php
+++ database/seeders/PetitionSeeder.php
@@ -14,38 +14,38 @@ class PetitionSeeder extends Seeder
     public function run(): void
     {
         Petition::create([
-            'fecha_hora' => '',
-            'accesosio_id' => '',
-            'equipo_id' => '',
-            'ingBiomedico_id' => '',
+            'fecha_hora' => now(),
+            'accesorio_id' => 1,
+            'equipo_id' => 1,
+            'ingBiomedico_id' => 6,
         ]);
 
         Petition::create([
-            'fecha_hora' => '',
-            'accesosio_id' => '',
-            'equipo_id' => '',
-            'ingBiomedico_id' => '',
+            'fecha_hora' => now(),
+            'accesorio_id' => 2,
+            'equipo_id' => 2,
+            'ingBiomedico_id' => 7,
             ]);
 
         Petition::create([
-            'fecha_hora' => '',
-            'accesosio_id' => '',
-            'equipo_id' => '',
-            'ingBiomedico_id' => '',
+            'fecha_hora' => now(),
+            'accesorio_id' => 3,
+            'equipo_id' => 3,
+            'ingBiomedico_id' => 8,
         ]);
 
         Petition::create([
-            'fecha_hora' => '',
-            'accesosio_id' => '',
-            'equipo_id' => '',
-            'ingBiomedico_id' => '',
+            'fecha_hora' => now(),
+            'accesorio_id' => 4,
+            'equipo_id' => 4,
+            'ingBiomedico_id' => 9,
         ]);
 
         Petition::create([
-            'fecha_hora' => '',
-            'accesosio_id' => '',
-            'equipo_id' => '',
-            'ingBiomedico_id' => '',
+            'fecha_hora' => now(),
+            'accesorio_id' => 5,
+            'equipo_id' => 4,
+            'ingBiomedico_id' => 10,
         ]);
     }
 }
