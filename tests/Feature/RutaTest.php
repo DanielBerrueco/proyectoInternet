ccaaf7eb814fc36479f0711c11fd52f8d2d05bc5
DanielBerrueco <dilan.sandoval0877@alumnos.udg.mx>
2024-05-08 14:16:59 -0600

    Test completo

--- tests/Feature/RutaTest.php
+++ tests/Feature/RutaTest.php
@@ -2,14 +2,14 @@
 
 namespace Tests\Feature;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 use App\Models\User;
 
 class RutaTest extends TestCase
 {
-
+    use DatabaseTransactions;
     /**
      * A basic feature test example.
      */
@@ -18,7 +18,7 @@ class RutaTest extends TestCase
         // Obtener el usuario por su email
         $user = User::where('email', 'example@test.com')->first();
 
-        dd($user);
+        //sdd($user);
 
         // Verificar que el usuario obtenido no sea null
         $this->assertNotNull($user, 'No se encontró el usuario con el correo electrónico proporcionado.');
