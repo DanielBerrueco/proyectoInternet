a85f4839f06025bc28978f063f398c1746bf5083
Sergio Gutierrez <sg7045167@gmailcom>
2024-05-08 09:57:24 -0600

    Corrección error ProfileController

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -16,26 +16,4 @@ class ProfileController extends Controller
     {
         return view('dashboard');
     }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request)
-    {
-        $request->validate([
-            'profile_photo' => 'image|mimes:jpeg,png,jpg,gif|max:2048',
-        ]);
-
-        $user = Auth::user();
-
-        if($request->hasFile('profile_photo')){
-            $imagePath = $request->file('profile_photo')->store('profile_photos', 'public');
-
-            $user->update(['profile_photo_path' => $imagePath]);
-        }
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
 }
